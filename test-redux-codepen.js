8cf2fa547bdfee4c0e2400ff76c78fb1379b71fe
milancez <milancez@gmail.com>
2018-10-15 16:32:39 +0200

    Setting up Redux in React

---
+++ test-redux-codepen.js
@@ -0,0 +1,31 @@
+const initState = {
+  todos: [],
+  posts: []
+}
+
+function myreducer(state = initState, action) {
+  if (action.type == 'ADD_TODO') {
+    return {
+      ...state,
+      todos: [...state.todos, action.todo]
+    }
+  }
+
+  if (action.type == 'ADD_POST') {
+    return {
+      ...state,
+      posts: [...state.posts, action.post]
+    }
+  }
+}
+
+const store = createStore(myreducer);
+
+store.subscribe(() => {
+  console.log('state updated');
+  console.log(store.getState());
+})
+
+store.dispatch({type: 'ADD_TODO', todo: 'buy milk'});
+store.dispatch({type: 'ADD_TODO', todo: 'sleep some more'});
+store.dispatch({type: 'ADD_POST', todo: 'egg hunt with yoshi'});
