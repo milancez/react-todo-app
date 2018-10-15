98886d702b290547a2ac9de1a9a93e78de131bc7
milancez <milancez@gmail.com>
2018-10-15 13:36:38 +0200

    Importing Images

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
+import CustomImg from '../images/react.jpg'
 
 class Home extends Component {
   state = { 
@@ -23,9 +24,10 @@ class Home extends Component {
       posts.map(post => {
         return (
           <div className="post card" key={post.id}>
+            <img src={CustomImg} alt="Custom React image"/>
             <div className="card-content">
               <Link to={'/' + post.id}>
-                <span className="card-title">{post.title}</span>
+                <span className="card-title red-text">{post.title}</span>
               </Link>
               <p>{post.body}</p>
             </div>
@@ -36,7 +38,7 @@ class Home extends Component {
       <div className="center">No posts yet</div>
     )
     return ( 
-      <div className="container">
+      <div className="container home">
         <h4 className="center">Home</h4>
         {postList}
       </div>
