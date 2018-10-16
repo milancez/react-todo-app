7682bbb68dbea78894e423f724504c4eafab1bb4
milancez <milancez@gmail.com>
2018-10-16 11:26:10 +0200

    Redux - Mapping State to Props

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+//import axios from 'axios'
 import { Link } from 'react-router-dom'
 import CustomImg from '../images/react.jpg'
+import { connect } from 'react-redux'
 
 class Home extends Component {
-  state = { 
+  /*state = { 
     posts: [ ]
    }
 
@@ -16,10 +17,12 @@ class Home extends Component {
           posts: res.data.slice(0,10) //samo 10 posta
         })
       })
-  }
+  }*/
 
   render() { 
-    const { posts } = this.state;
+    console.log(this.props)
+    //const { posts } = this.state;
+    const { posts } = this.props;
     const postList = posts.length ? (
       posts.map(post => {
         return (
@@ -45,5 +48,11 @@ class Home extends Component {
      );
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    posts: state.posts
+  }
+}
  
-export default Home;
+export default connect(mapStateToProps)(Home);
