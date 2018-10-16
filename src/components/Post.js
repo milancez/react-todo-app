d9b64b23d27bcca4cbd7575894050e188013ee00
milancez <milancez@gmail.com>
2018-10-16 13:58:44 +0200

    Map Dispatch To Props

--- src/components/Post.js
+++ src/components/Post.js
@@ -18,14 +18,25 @@ class Post extends Component {
       })
   }*/
 
+  handleClick = () => {
+    this.props.deletePost(this.props.post.id);
+    this.props.history.push('/');
+
+  }
+
   render() { 
-    //console.log(this.props);
+    console.log(this.props);
 
     //const post = this.state.post ? (
     const post = this.props.post ? (
       <div className="post">
         <h4 className="center">{this.props.post.title}</h4>
         <p>{this.props.post.body}</p>
+        <div className="center">
+          <button className="btn grey" onClick={this.handleClick}>
+            Delete Post
+          </button>
+        </div>
       </div>
     ) : (
       <div className="center">Loading post...</div>
@@ -49,5 +60,11 @@ const mapStateToProps = (state, ownProps) => {
     post: state.posts.find( post => post.id == id )
   }
 }
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    deletePost: (id) => { dispatch({ type: 'DELETE_POST', id: id }) }
+  }
+}
  
-export default connect(mapStateToProps)(Post)
+export default connect(mapStateToProps, mapDispatchToProps)(Post)
